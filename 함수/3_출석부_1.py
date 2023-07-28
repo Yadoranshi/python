5385c094f417f6d1c7c9ff4192fae27b583cd4a4
tkfkdgodhd1@gmail.com <tkfkdgodhd1@gmail.com>
2023-07-28 21:19:15 +0900

    .

--- 함수/3_출석부_1.py
+++ 함수/3_출석부_1.py
@@ -1,9 +1,17 @@
 def add_student(attendance, student):
+    if student in attendance:
+        
     attendance.append(student)
     return
 
 def remove_student(attendance, student):
-
+    attendance.remove(student)
+    return
 
 
 attendance = []
+
+add_student(attendance, ['유규민','17번','남자'])
+add_student(attendance, ['유규민','17번','남자'])
+remove_student(attendance, ['유규민', '17번', '남자'])
+print(attendance)
